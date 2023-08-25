f5acd07b5e3bdf66e3575c4467954d684637a589
Antonius Torode <awtorode@gmail.com>
2023-08-25 17:54:57 -0500

    Updating updateSubmodule script

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,3 +1,9 @@
 git submodule update --recursive --remote
-git pull && git submodule foreach git pull origin master
+git pull && git submodule foreach git pull origin main
+cd database
+git pull origin main
+cd ..
+git add database
+git commit -m "Updating submodule to latest commit"
+git push origin main
 
